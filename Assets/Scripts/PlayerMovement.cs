e38f7f40d5d939a47630c0c3dfd705f8c2dd2561
Zohaib Shaukat <zohaibshaukat000@gmail.com>
2023-05-04 12:27:03 +0500

    Added Collectible items to the MVP

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	private Rigidbody2D rb;
+	private BoxCollider2D coll;
 	private Animator anim;
 	private SpriteRenderer sprite;
 	private float dirX = 0f;
 	[SerializeField] private float moveSpeed = 7f;
 	[SerializeField] private float jumpForce = 14f;
+	[SerializeField] private LayerMask jumpableGround;
 	private enum MovementState {idle, running, jumping, falling};
 	
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+		coll = GetComponent<BoxCollider2D>();
 		anim = GetComponent<Animator>();
 		sprite = GetComponent<SpriteRenderer>();
     }
@@ -27,7 +30,7 @@ public class PlayerMovement : MonoBehaviour
     {
 	dirX = Input.GetAxisRaw("Horizontal");
 	rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-	if (Input.GetButtonDown("Jump"))
+	if (Input.GetButtonDown("Jump")&& IsGrounded())
 	{
 		rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 	}
@@ -59,5 +62,9 @@ public class PlayerMovement : MonoBehaviour
 		state = MovementState.falling;
 	}
 	anim.SetInteger("state", (int)state);
+	}
+	private bool IsGrounded()
+	{
+		return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
 	}
 }
