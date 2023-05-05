56d4be9038593ad076aa6e8cd3a40bbc22518907
Zohaib Shaukat <zohaibshaukat000@gmail.com>
2023-05-05 20:20:38 +0500

    Updated collectible items

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ItemCollector : MonoBehaviour
 {
 	private int cherries = 0;
@@ -10,7 +11,7 @@ public class ItemCollector : MonoBehaviour
 		  if(collision.gameObject.CompareTag("Cherry"))
 		  {
 			  Destroy(collision.gameObject);
-			  cherries++;
+			 
 		  }
 	 }
 	 
